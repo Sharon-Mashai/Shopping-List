9dce1302f2271a9fe5570083be56fac72a3b9a89
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-08-23 17:39:06 +0200

    Created the authentication slice in order to use it for protected routes

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  id: string;
+  name: string;
+  surname: string;
+  email: string;
+  cellNumber: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isLoggedIn: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+    },
+
+    logout: (state) => {
+      state.user = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
