27e61100cec79b5de220b3afa7c631966a695f04
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-08-23 18:39:51 +0200

    Updated the app.tsx file to place the home, shopping list and profile routes under protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import {BrowserRouter,Routes,Route,Navigate,} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ShoppingList from "./pages/ShoppingList";
 import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/" element={<Navigate to="/login" />}
+        />
 
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login" element={<Login />}
+        />
 
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/register" element={<Register />}
+        />
 
-        <Route path="/home" element={<Home />} />
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/home" element={<Home />}
+          />
 
-        <Route path="/shopping-list/:id" element={<ShoppingList />} />
+          <Route
+            path="/shopping-list/:id" element={<ShoppingList />}
+          />
 
-        <Route path="/profile" element={<Profile />} />
-        
+          <Route
+            path="/profile" element={<Profile />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
