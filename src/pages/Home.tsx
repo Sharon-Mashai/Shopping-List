a709e6789f47c44878dbe3c468c1d005e1834e99
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-08-24 18:37:48 +0200

    Added an empty state image in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import EmptyStateImage from "../assets/EmptyStateImage.png";
+
 function Home() {
   return (
-    <main>
-      <h1>Home Page</h1>
+    <main className="home-page">
+      <section className="page-header">
+        <div>
+          <h1>My Shopping Lists</h1>
+        </div>
+
+      </section>
+
+      <section className="empty-state">
+        <div className="empty-state"> <img
+              src={EmptyStateImage} alt="empty-state"/>
+        </div>
+
+        <h2>Your shopping list is empty</h2>
+
+        <p>
+          Start by creating your first shopping list.
+        </p>
+
+        <button className="button button-primary">
+          Create Shopping List
+        </button>
+      </section>
     </main>
   );
 }
