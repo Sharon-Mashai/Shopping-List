38f464328fffd4504c15eb385245c0e82238c949
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-08-24 18:58:04 +0200

    Connected the login and register pages to the public route component under app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,31 @@ import Home from "./pages/Home";
 import ShoppingList from "./pages/ShoppingList";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/" element={<Navigate to="/login" />}
+          path="/"
+          element={<Navigate to="/login" />}
         />
 
-        <Route
-          path="/login" element={<Login />}
-        />
-
-        <Route
-          path="/register" element={<Register />}
-        />
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />}/>
+          
+          <Route path="/register"element={<Register />} />
+        </Route>
 
         <Route element={<ProtectedRoute />}>
-          <Route
-            path="/home" element={<Home />}
-          />
+          <Route path="/home" element={<Home />} />
 
-          <Route
-            path="/shopping-list/:id" element={<ShoppingList />}
-          />
+          <Route path="/shopping-list/:id" element={<ShoppingList />} />
 
-          <Route
-            path="/profile" element={<Profile />}
-          />
+          <Route path="/profile" element={<Profile />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
