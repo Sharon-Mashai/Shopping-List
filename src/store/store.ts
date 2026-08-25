75f2a812faa2c7bfa92da35a80e01c9a1fb6fd39
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-08-25 10:00:47 +0200

    Added the shopping items slice to the redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import authReducer from "./slices/authSlice";
-import shoppingListReducer from "./slices/ShoppingListSlice"
+import shoppingListReducer from "./slices/ShoppingListSlice";
+import shoppingItemReducer from "./slices/ShoppingItemsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     shoppingLists: shoppingListReducer,
+    shoppingItems: shoppingItemReducer,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<
+  typeof store.getState
+>;
+
 export type AppDispatch = typeof store.dispatch;
